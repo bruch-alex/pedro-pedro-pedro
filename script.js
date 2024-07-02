5ead4ce3ad695bd773fbbe98777e4f1d55d8fece
alexbruch2024 <alex.bruch@dci-student.org>
2024-07-02 15:55:05 +0200

    handle mobile touchscreens

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let image = document.querySelector("img")
-
 let body = document.querySelector("body")
 let counter = 0
-function handleMouseMoveEvent(event){
-    counter += event.deltaY/10
+
+if (navigator.userAgent.match(/(iPad)|(iPhone)|(iPod)|(android)|(webOS)/i)) {
+    document.addEventListener("touchmove", handleMoveEvent)
+}
+else {
+    document.addEventListener("wheel", handleMoveEvent)
+}
+
+function handleMoveEvent(event) {
+    counter += event.deltaY * 0.1
     console.log(counter)
 
     image.style.rotate = counter + "deg"
     let hue = counter
-    let sat = counter
-    body.style.background = "hsl(" + hue + ", " + sat + "%, 30%)"
+    body.style.background = "hsl(" + hue + ", 70%, 30%)"
 }
-// document.addEventListener("mousemove", handleMouseMoveEvent)
-document.addEventListener("wheel", handleMouseMoveEvent)
